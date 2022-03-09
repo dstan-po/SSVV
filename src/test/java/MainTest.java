460c5d804b10f77b1f522a92832fbcb19a722bb4
danie <danielstanpop@gmail.com>
2022-03-09 18:01:47 +0200

    Dedeted

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class MainTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void shouldAnswerWithTrueTest()
+    {
+        assertTrue( false );
+    }
+}
