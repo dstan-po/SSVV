afe357613acac36bbaa73ce4cbd4922986a45268
danie <danielstanpop@gmail.com>
2022-03-21 20:01:17 +0200

    Broken project

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -2,8 +2,8 @@ import domain.Nota;
 import domain.Student;
 import domain.Tema;
 import org.junit.Before;
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import repository.NotaXMLRepository;
 import repository.StudentXMLRepository;
 import repository.TemaXMLRepository;
@@ -16,70 +16,65 @@ import validation.Validator;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Iterator;
 
 public class StudentTest {
-    public static Service service;
 
-    public StudentTest() {
-    }
+    public static Service service;
 
     @Before
     public void setup() {
         Validator<Student> studentValidator = new StudentValidator();
         Validator<Tema> temaValidator = new TemaValidator();
         Validator<Nota> notaValidator = new NotaValidator();
+
+
         StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "test_studenti.xml");
         TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "test_teme.xml");
         NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "test_note.xml");
+
         service = new Service(fileRepository1, fileRepository2, fileRepository3);
     }
 
     @Test
-    public void Test_AddStudent_Success_Test() {
-        Student newStudent = new Student("2822", "Ionel", 32);
-        service.saveStudent("2822", "Ionel", 222);
+    public void Test_addStudent_Success_Test() {
+        Student newStudent = new Student("2822", "Ionel", 223);
+        service.saveStudent("28223", "Ionel", 223);
+        System.out.println("BEFORE");
         Iterable<Student> newStudentList = service.findAllStudents();
-        Iterator var3 = newStudentList.iterator();
 
-        Student student;
-        do {
-            if (!var3.hasNext()) {
-                assert false;
-
+        for (Student student : newStudentList) {
+            if (student.equals(newStudent))
                 return;
-            }
-
-            student = (Student) var3.next();
-        } while (!student.equals(newStudent));
-
+        }
+        assert false;
     }
 
     @Test
-    public void Test_AddStudent_Fail_Test() {
+    public void Test_addStudent_Fail_Test() {
         Iterable<Student> initialStudentList = service.findAllStudents();
+
         service.saveStudent("2822", "Ionel", 50);
         Iterable<Student> newStudentList = service.findAllStudents();
 
         assert initialStudentList.equals(newStudentList);
-
     }
 
     private void writeEmptyXMLFile(String filenameWithExtension, String mainEntityBracket) {
         try {
             File myObj = new File(filenameWithExtension);
+
             if (!myObj.createNewFile()) {
                 FileWriter myWriter = new FileWriter(filenameWithExtension);
                 myWriter.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n<" + mainEntityBracket + ">\n</" + mainEntityBracket + ">");
                 myWriter.close();
             }
-        } catch (IOException var5) {
-            var5.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     @BeforeEach
     public void resetFiles() {
-        this.writeEmptyXMLFile("test_studenti.xml", "Entitati");
+        writeEmptyXMLFile("test_studenti.xml", "Entitati");
     }
 }
